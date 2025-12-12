4355fe20f85200cff3dde5961f9440dc810fd544
Zain Shk <zshk599@gmail.com>
2025-12-13 01:13:43 +0500

    Home Video

--- components/sections/HomeVideo.jsx
+++ components/sections/HomeVideo.jsx
@@ -2,21 +2,21 @@ import React from "react";
 
 const HomeVideo = () => {
   return (
-    <div className="relative" >
-
-    <div className="bg-black/20 h-screen inset-0 overflow-hidden">
-      <video
-        className="w-full h-full object-cover"
-        src="/homevideo.mp4"
-        autoPlay
-        muted
-        loop
-        playsInline
-      />
-      {/* subtle parallax effect */}
-      <div className="bg-black/30 inset-0 absolute " ></div>
+    <div className="relative lg:px-24">
+      <div className="bg-black/20 lg:h-screen inset-0 overflow-hidden">
+        <video
+          className="w-full h-full object-contain"
+          src="/hamail-homevideo.mp4"
+          autoPlay
+          muted
+          controls
+          loop
+          playsInline
+        />
+        {/* subtle parallax effect */}
+        {/* <div className="bg-black/30 inset-0 absolute " ></div> */}
+      </div>
     </div>
-        </div>
   );
 };
 
